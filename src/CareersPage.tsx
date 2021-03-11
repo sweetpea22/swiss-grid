e9e51fcf0580143ceb0f3992d5d825a23c6473cf
sweetpea22 <cindychau24@me.com>
2021-03-11 09:47:42 -0500

    adaptive content grid

--- src/CareersPage.tsx
+++ src/CareersPage.tsx
@@ -4,7 +4,8 @@ import { createStyles, ITheme, makeStyles } from "@chainsafe/common-theme";
 
 const useStyles = makeStyles(({ palette, breakpoints, constants }: ITheme) => {
   return createStyles({
-    pageContainer: {},
+    pageContainer: {
+    },
     contentContainer: {
     },
     headerContainer: {
@@ -21,7 +22,7 @@ const useStyles = makeStyles(({ palette, breakpoints, constants }: ITheme) => {
       [breakpoints.down('md')]: {
         justifyItems: "center",
         gridTemplateColumns: "1fr",
-        margin: "30px 24px 24px 24px",
+        margin: "30px 24px 80px 24px",
       },
       [breakpoints.up('md')]: {
         gridTemplateColumns: "35% 1fr",
@@ -29,7 +30,7 @@ const useStyles = makeStyles(({ palette, breakpoints, constants }: ITheme) => {
         margin: "0 5% 0 5%",
       },
       [breakpoints.up('lg')]: {
-        gridTemplateColumns: "35% 1fr%",
+        gridTemplateColumns: "35% 1fr",
         columnGap: "20px",
         margin: "0 5% 0 5%",
       },
@@ -75,28 +76,33 @@ const useStyles = makeStyles(({ palette, breakpoints, constants }: ITheme) => {
       display: "grid",
       gridGap: "24px",
       [breakpoints.up('md')]: {
-        gridTemplateColumns: "minmax(0, 1fr)"
+        gridTemplateColumns: "minmax(0, 1fr)",
+        marginTop: "24px",
       },
       [breakpoints.up('lg')]: {
-        gridTemplateColumns: "minmax(0,6fr) minmax(0,.5fr) minmax(0, 6fr)",
+        gridTemplateColumns: "minmax(0,5fr) minmax(0,.5fr) minmax(0, 5fr)",
         marginTop: "5%",
       }
     },
-    bodyTextContainer: { height: "50vh", background: palette.additional["gray"][3],
-    position: "relative",
-    width: "100%",
-    [breakpoints.up('lg')]: {
-      padding: "48px 8px 48px 8px",
-    },
+    bodyTextContainer: {
+      background: palette.additional["gray"][3],
+      position: "relative",
+      width: "100%",
+      [breakpoints.down('md')]: {
+        minHeight: "50vh",
+        padding: "48px 8px 48px 8px",
+      },
+      [breakpoints.up('md')]: {
+        height: "45vh",
+        padding: "48px 8px 48px 8px",
+      },
+      [breakpoints.up('lg')]: {
+        padding: "48px 8px 48px 8px",
+        height: "50vh",
+      },
     },
     hr: {
-      background: "darkgray",
-    },
-    gridline: {
-      background: palette.additional["gray"][6],
-      height: "1px",
-      minWidth: "500px",
-      maxWidth: "100%",
+      background: palette.additional["gray"][3],
     },
     bodyTextWrapper: {
       [breakpoints.up('lg')]: {
@@ -105,12 +111,13 @@ const useStyles = makeStyles(({ palette, breakpoints, constants }: ITheme) => {
       },
       "& > p": {
         color: palette.additional["gray"][9],
-        fontSize: "14px",
+        fontSize: "16px",
         lineHeight: "24px",
+        maxWidth: "60%",
       },
       "& > a": {
         color: palette.additional["gray"][9],
-        fontSize: "14px",
+        fontSize: "16px",
         lineHeight: "24px",
       },
       "& > h1": {
@@ -120,15 +127,27 @@ const useStyles = makeStyles(({ palette, breakpoints, constants }: ITheme) => {
       },
     },
     topAlignedList: {
-        position: "absolute",
+      position: "absolute",
       top: 0,
-        width: "100%",
+      width: "100%",
+      [breakpoints.down('md')]: {
+        gridRow: 1,
+      },
       "& > h1": {
           color: palette.additional["gray"][9],
           fontSize: "20px",
           padding: "8px",
           lineHeight: "44px",
     }
+    },
+    imgWrapper: {
+      position: "absolute", top: 0, height: "100%", width: "auto",
+      [breakpoints.down('lg')]: {
+        width: "100%",
+      },
+      "& > img": {
+        width: "100%",
+      }
     },
   })
 })
@@ -146,11 +165,12 @@ const CareersPage: React.FC = () => {
             </div>
         </div>
       </header>
-      <main className={classes.bodyContainer}>
+        <main className={classes.bodyContainer}>
+        {/* Intro */}
         <section className={classes.bodyContentContainer}>
           <div className={classes.bodyTextContainer}>
-            <div style={{ background: "#141414", position: "absolute", top: 0, height: "100%", width:"100%"}}>
-            <img src="/while.png" style={{maxWidth: "100%", maxHeight: "100%"}} alt=""/>
+            <div className={classes.imgWrapper}>
+            <img src="/plant.png" style={{width: "auto", height: "100%"}} alt=""/>
             </div>
           </div>
           <div></div>
@@ -162,10 +182,12 @@ const CareersPage: React.FC = () => {
               <p>Our mission is to support the future of open-source software and to bring the power of trustless systems to its greatest potential. </p>
               <br></br>
               <p>We do this by delivering high-quality code efficiently, and doing that in a people-first manner.</p>
+              <br></br>
               <a href="/openpositions"><h3>View open positions</h3></a>
             </div>
           </div>
           </section>
+          {/* Get to know us */}
           <section className={classes.bodyContentContainer}>
           <div className={classes.bodyTextContainer}>
             <div className={classes.topAlignedList}>
@@ -173,18 +195,67 @@ const CareersPage: React.FC = () => {
               <hr className={classes.hr}></hr>
               <h1><a href="/openpositions">ChainSafers, interviewed: Dustin Brickwood</a></h1>
               <hr className={classes.hr}></hr>
-              <h1>ChainSafers, interviewed: Ed Mack </h1>
+              <h1><a href="/openpositions">ChainSafers, interviewed: Ed Mack</a></h1>
               <hr className={classes.hr}></hr>
-              <h1>ChainSafers, interviewed: Elizabeth Binks </h1>
+              <h1><a href="/openpositions">ChainSafers, interviewed: Elizabeth Binks</a></h1>
               <hr className={classes.hr}></hr>
-              <h1>The story behind ChainSafe </h1>
+              <h1><a href="/openpositions">The story behind ChainSafe</a></h1>
+              <hr className={classes.hr}></hr>
+              <h1><a href="/openpositions">How we support employees</a></h1>
               <hr className={classes.hr}></hr>
             </div>
           </div>
           <div></div>
           <div className={classes.bodyTextContainer}>
-            <div style={{ height: "100%", width:"100%"}}>
-              <img src="/peak.jpeg" style={{maxWidth: "100%", maxHeight: "100%"}} alt=""/>
+            <div className={classes.imgWrapper}>
+            <img src="/plant.png" style={{width: "auto", height: "100%"}} alt=""/>
+            </div>
+          </div>
+          </section>
+          {/* Culture */}
+          <section className={classes.bodyContentContainer}>
+          <div className={classes.bodyTextContainer}>
+            <div className={classes.bodyTextWrapper}>
+              <h1>Culture is number one.</h1>
+              <hr className={classes.hr}></hr>
+              <br></br>
+              <p>Great cultures make better organizations, which make better societies. ChainSafe is strongly committed to maintaining a welcoming environment for everyone to do their best work. </p>
+              <br></br>
+              <p>There are ChainSafers on every continent. That means creating a space of  diversity, inclusion, and belonging is second to none. </p>
+              <br></br>
+              <h1><a href="/openpositions">Read about our value system</a></h1>
+
+            </div>
+          </div>
+          <div></div>
+          <div className={classes.bodyTextContainer}>
+            <div className={classes.imgWrapper} style={{background: "gray", width: "100%"}}>
+              <h1>hello?</h1>
+            </div>
+          </div>
+          </section>
+          {/* Current Openings */}
+          <section className={classes.bodyContentContainer}>
+          <div className={classes.bodyTextContainer}>
+            <div className={classes.topAlignedList}>
+              <p>Current Openings Worldwide </p>
+              <hr className={classes.hr}></hr>
+              <h1><a href="/openpositions">Go Developer</a></h1>
+                <hr className={classes.hr}></hr>
+              <h1><a href="/openpositions">Rust Developer</a></h1>
+              <hr className={classes.hr}></hr>
+              <h1><a href="/openpositions">Typescript Ethereum Developer</a></h1>
+              <hr className={classes.hr}></hr>
+              <h1><a href="/openpositions">Solutions Engineer</a></h1>
+              <hr className={classes.hr}></hr>
+              <h1><a href="/openpositions">Don't see your role? Apply here</a></h1>
+              <hr className={classes.hr}></hr>
+            </div>
+          </div>
+          <div></div>
+          <div className={classes.bodyTextContainer}>
+              <div style={{ position: "absolute", top: 0, height: "100%", width: "auto" }}>
+              <img src="/plant.png" style={{width: "auto", height: "100%"}} alt=""/>
               </div>
           </div>
         </section>
